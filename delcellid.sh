d2a25183b69fc4ea49b9c8bf9d41d86ffc10ae3f
Taro Yabuki <taroyabuki@users.noreply.github.com>
2024-03-17 00:41:57 +0900

    P

--- delcellid.sh
+++ delcellid.sh
@@ -1,7 +1,13 @@
-cat python.ipynb | jq 'del(.cells[].id)' > tmp
+cat python.ipynb \
+| jq 'walk(if type == "object" then del(.id) else . end)' \
+| jq 'walk(if type == "object" and .metadata == {} then del(.metadata) else . end)' \
+> tmp
 rm python.ipynb
 mv tmp python.ipynb
 
-cat python-results.ipynb | jq 'del(.cells[].id)' > tmp
+cat python-results.ipynb \
+| jq 'walk(if type == "object" then del(.id) else . end)' \
+| jq 'walk(if type == "object" and .metadata == {} then del(.metadata) else . end)' \
+> tmp
 rm python-results.ipynb
 mv tmp python-results.ipynb
